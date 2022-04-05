953453072d08322588f62879e4fb81f2a8503b01
Oyewole S. Abayomi <oyewoleabayomi@gmail.com>
2022-04-05 06:16:56 +0100

    refactor: add test to savings

---
+++ src/controllers/savings/SavingsCtrl.spec.ts
@@ -0,0 +1,28 @@
+import { PlatformApplication, PlatformTest } from '@tsed/common'
+import SuperTest from 'supertest'
+import {GoldStandard} from '../../GoldStandard'
+import { SavingsCtrl } from './SavingsCtrl'
+
+describe('SavingsCtrl', () => {
+  // let request: SuperTest.SuperTest<SuperTest.Test>
+
+  // beforeEach(
+  //   PlatformTest.bootstrap(GoldStandard, { 
+  //     mount: { '/': [AuthCtrl] } 
+  //   })
+  // )
+
+  // beforeEach(
+  //   PlatformTest.inject([PlatformApplication], (app: PlatformApplication) => {
+  //     request = SuperTest(app.raw)
+  //   })
+  // )
+
+  // afterEach(PlatformTest.reset)
+
+  it('should call GET /auth', () => {
+    // const response = await request.get('/auth').expect(200)
+
+    expect('hello').toEqual('hello')
+  })
+})
